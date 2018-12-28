a034e3aa1ccd2f85d81661259e91cb531aaf1c76
bobbyjey@gmail.com <bobby.choi@hcs.com>
2018-12-28 10:53:46 +0800

    add alow host setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sql#84z^=#f=3+(bck9=*0)u&)6m=pph%(wupb1v%(wybee)+='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bobbyjey.pythonanywhere.com']
 
 
 # Application definition
